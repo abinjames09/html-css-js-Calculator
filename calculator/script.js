b4bb10725b0129c138438d30d72436826aecfd42
abinjames09 <110019299+abinjames09@users.noreply.github.com>
2022-07-26 13:20:33 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,17 @@
+function bclick(val)
+{
+    document.getElementById("screen").value+=val
+}
+
+function bclear()
+{
+    document.getElementById("screen").value=""
+
+}
+
+function eqclick()
+{
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
